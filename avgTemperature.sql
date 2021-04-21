f238cb8e3752fc06119cd36571c1141e3c59e016
Alexey Novakov <novakov.alex@gmail.com>
2021-04-21 10:10:35 +0200

    fix name of the source stream

--- avgTemperature.sql
+++ avgTemperature.sql
@@ -9,12 +9,12 @@
 -- PUMP: an entity used to continuously 'SELECT ... FROM' a source STREAM, and INSERT SQL results into an output STREAM
 -- Create output stream, which can be used to send to a destination
 CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM" ("number" INTEGER, avg_temperature double);
--- Create a pump which continuously selects from a source stream (SOURCE_SQL_STREAM_001)
+-- Create a pump which continuously selects from a source stream (sensor-temperature_001)
 -- performs an aggregate count that is grouped by columns ticker over a 10-second tumbling window
 -- and inserts into output stream (DESTINATION_SQL_STREAM)
 CREATE OR REPLACE  PUMP "STREAM_PUMP" AS INSERT INTO "DESTINATION_SQL_STREAM"
 -- Aggregate function COUNT|AVG|MAX|MIN|SUM|STDDEV_POP|STDDEV_SAMP|VAR_POP|VAR_SAMP)
 SELECT STREAM "number", AVG("temperature") AS avg_temperature
-FROM "SOURCE_SQL_STREAM_001"
+FROM "sensor-temperature_001"
 -- Uses a 10-second tumbling time window
-GROUP BY "number", FLOOR(("SOURCE_SQL_STREAM_001".ROWTIME - TIMESTAMP '1970-01-01 00:00:00') SECOND / 10 TO SECOND);
+GROUP BY "number", FLOOR(("sensor-temperature_001".ROWTIME - TIMESTAMP '1970-01-01 00:00:00') SECOND / 10 TO SECOND);
